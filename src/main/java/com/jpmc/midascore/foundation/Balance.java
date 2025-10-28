be2c7dd3ee637f419dc4ee2d6853491fa767669a
Ankit22011999 <arnik0307@gmail.com>
2025-10-29 00:31:16 +0530

    Use more annotation
    same issue that i commit in transaction.java file

--- src/main/java/com/jpmc/midascore/foundation/Balance.java
+++ src/main/java/com/jpmc/midascore/foundation/Balance.java
@@ -1,25 +1,30 @@
 package com.jpmc.midascore.foundation;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Balance {
     private float amount;
 
-    public Balance() {
-    }
+    // public Balance() {
+    // }
 
-    public Balance(float amount) {
-        this.amount = amount;
-    }
+    // public Balance(float amount) {
+    //     this.amount = amount;
+    // }
 
-    public float getAmount() {
-        return amount;
-    }
+    // public float getAmount() {
+    //     return amount;
+    // }
 
-    public void setAmount(float amount) {
-        this.amount = amount;
-    }
+    // public void setAmount(float amount) {
+    //     this.amount = amount;
+    // }
 
     @Override
     public String toString() {
